98f9188773ccf65b37f11272576007b5b4e6bdfb
Maruf <maruffnadaf@gmail.com>
2026-01-28 00:31:39 +0530

    chore: add comment to button component

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+// Basic button component based on shadcn/ui
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
