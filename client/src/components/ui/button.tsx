58d7108e1b84771be723d4a028e0184a7bc5e791
Maruf <maruffnadaf@gmail.com>
2026-01-30 00:01:50 +0530

    chore: add utility comment

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
+// Utility for class name merging
 import { cn } from "@/lib/utils"
 
 // Button variants configuration
