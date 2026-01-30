7acdfd3813b82923db4af19fa0712aaf1fd1409f
Maruf <maruffnadaf@gmail.com>
2026-01-31 01:47:15 +0530

    chore: add badge props comment

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -27,6 +27,7 @@ const badgeVariants = cva(
   },
 )
 
+// Props interface for Badge
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
   VariantProps<typeof badgeVariants> { }
