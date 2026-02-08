2323f34e57bf9b55e2e6d2c5bd9272547a8cf42e
Maruf <maruffnadaf@gmail.com>
2026-02-08 23:22:43 +0530

    Remove comment from badge.tsx

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-// Utility for class merging
 import { cn } from "@/lib/utils"
 
 // Badge variants configuration
