aebb82a3ab762d457c6beb8ce46f6a96ff89a246
Maruf <maruffnadaf@gmail.com>
2026-02-12 22:29:54 +0530

    Remove comment from badge.tsx

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -16,7 +16,6 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow-xs",
 
-        // Outline variant style
         outline: " border [border-color:var(--badge-outline)] shadow-xs",
       },
     },
