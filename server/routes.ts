348a9bd50b80a4ca59821c64c764b8f2f205d6e2
Maruf Nadaf <153378078+MarufxAlchemist@users.noreply.github.com>
2026-04-16 22:40:15 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -7,7 +7,7 @@ export async function registerRoutes(
   app: Express
 ): Promise<Server> {
   // put application routes here.
-  // prefix all routes with /api
+  // prefix all routes with /api.
 
   // use storage to perform CRUD operations on the storage interface
   // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
