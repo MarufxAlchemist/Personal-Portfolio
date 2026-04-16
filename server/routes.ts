b08c229b23465d9bde89e865275dde23347c2bf8
Maruf Nadaf <153378078+MarufxAlchemist@users.noreply.github.com>
2026-04-16 22:40:23 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -9,7 +9,7 @@ export async function registerRoutes(
   // put application routes here.
   // prefix all routes with /api.
 
-  // use storage to perform CRUD operations on the storage interface
+  // use storage to perform CRUD operations on the storage interface.
   // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
 
   return httpServer;
